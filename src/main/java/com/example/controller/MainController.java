8d077c2d9e95efc9bfaf98cc3a956b50e9323b9e
tamaitamai <yktmi0217@gmail.com>
2023-07-06 15:24:09 +0900

    謎解き初回

---
+++ src/main/java/com/example/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.repository.MainRepository;
+
+@Controller
+@RequestMapping("main")
+public class MainController {
+	@Autowired
+	MainRepository mainRepository;
+	
+	@GetMapping("")
+	public String main() {
+		mainRepository.findAll().forEach(System.out::println);
+		return "main/main.html";
+	}
+}
